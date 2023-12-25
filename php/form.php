b9838446fd35c2f82353e4fe30dc3609d92bd3d6
Pchiroli <pablochiroli@hotmail.com>
2023-12-25 13:45:25 -0300

    ajuste de form para envio mail y ajuste de css

---
+++ php/form.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$nombre = $_POST["nombre"];
+$mail = $_POST["mail"];
+$telefono = $_POST["telefono"];
+$mensaje = $_POST["mensaje"];
+
+
+//como va a llegar el cuerpo del mensaje
+$mensaje = "Este mensaje fue enviado por" . $nombre . ",\r\n";
+$mensaje .= "Su email es" . $mail . ",\r\n";
+$mensaje .= "Su numero de telefono es" . $telefono . ",\r\n";
+$mensaje .= "Mensaje" . $_POST['mensaje'] . ",\r\n";
+$mensaje .= "Enviado el" . date('d/m/Y', time());
+
+$para = 'pablochiroli@hotmail.com';
+$asunto = 'Este mail fue enviado de la Web de La Colmena'
+
+//funcion mail
+//a quien le mando el email
+mail($para, $asunto, utf8_decode($mensaje), $header)
+header('location:index.html')
